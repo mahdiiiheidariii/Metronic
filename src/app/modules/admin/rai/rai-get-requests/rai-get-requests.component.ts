0266bfc98b143b11d4032746564be44547a6c257
maryam rajabi <mahdiiheidari.a@gmail.com>
2019-08-17 14:07:07 -0700

    a

--- src/app/modules/admin/rai/rai-get-requests/rai-get-requests.component.ts
+++ src/app/modules/admin/rai/rai-get-requests/rai-get-requests.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class RaiGetRequestsComponent implements OnInit {
 
     items = [
-        {id: 0, requestNo: 'بازخورد ', requestType: 'داخلی', startStation: 'درخواست 1', endStation: 'پیشنهاد 1', requestDate: '1398/14/03', status: 1},
-        {id: 1, requestNo: 'بازخورد', requestType: 'داخلی', startStation: 'درخواست 1', endStation: 'انتقاد 1', requestDate: '1398/14/03', status: 5},
-        {id: 2, requestNo: 'بازخورد', requestType: 'داخلی', startStation: 'درخواست 1', endStation: 'شکایت 1', requestDate: '1398/14/03', status: 3},
+        {id: 0, requestNo: 'درخواست کننده', requestType: 'عنوان شرکت', startStation: 'داخلی', endStation: 'مبدا 1', requestDate: '1398/14/03', status: 1},
+        {id: 1, requestNo: 'درخواست کننده', requestType: 'عنوان شرکت', startStation: ' داخلی', endStation: 'مبدا 1', requestDate: '1398/14/03', status: 5},
+        {id: 2, requestNo: 'درخواست کننده', requestType: 'عنوان شرکت', startStation: ' داخلی', endStation: 'مبدا 1', requestDate: '1398/14/03', status: 3},
     ];
     constructor() { }
 
